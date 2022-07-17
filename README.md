65b82d7b40a5230d12aa8134d2b9b8b0926c9eb9
galipyildiz <galipyildiz9606@outlook.com>
2022-07-17 03:31:28 +0300

    tests and raedme file

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# TcIdentityNumber
+<p align="center">
+<b>Check TC Identity Number</b>
+<br />
+<img src="./identityChecker.png"/>
+<br />
+Verify TC Identity Number the easiest way
+</p>
+
+----
+### How to use?
+- Install nuget package
+  - Package Manager Console
+    ```
+      Install-Package TcIdentityNumber -Version 1.0.0 
+    ``` 
+  - Nuget Package Solution
+    ![](./assets/installnugetpackage.png)
+    Select this package and install your project.
+- Enjoy your verify identity number 
+    - .Net 6.0 Console Application
+    ```cs
+    using TCIdentityNumber;
+
+    long identityNumber = 1111111111;
+    Console.WriteLine(identityNumber.IsIdentityNumberCorrect());
+    Console.WriteLine(await identityNumber.IsIdentityNumberCorrectAsync("Ali", "Veli", 2022));
+    ```
+    - Output
+    ```
+    False
+    False
+    ```
+
+**Note:** If you are correct information, you can reach the correct results. :)
---
+++ TcIdentityNumber.Tests/TestTCIdentityNumberHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using TCIdentityNumber;
+
+namespace TcIdentityNumber.Tests
+{
+    [TestFixture]
+    public class TestTCIdentityNumberHelper
+    {
+        [Test]
+        public void IsIdentityNumberCorrectWithIdentityNumberDigitsLessThanEleven_ReturnFalse()
+        {
+            bool result = TCIdentityNumberHelper.IsIdentityNumberCorrect(111);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsIdentityNumberCorrectWithIdentityNumberDigitsGreaterThanEleven_ReturnFalse()
+        {
+            bool result = TCIdentityNumberHelper.IsIdentityNumberCorrect(111111111111);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsIdentityNumberCorrectWithIdentityNumberStartWithZero_ReturnFalse()
+        {
+            bool result = TCIdentityNumberHelper.IsIdentityNumberCorrect(0111);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task IsIdentityNumberCorrectAsyncWithIdentityNumberDigitsLessThanEleven_ReturnFalse()
+        {
+            bool result = await TCIdentityNumberHelper.IsIdentityNumberCorrectAsync(51529486516, "Ali", "Veli", 2000);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task IsIdentityNumberCorrectAsyncWithBirthYearDigitsLessThanFour_ReturnFalse()
+        {
+            bool result = await TCIdentityNumberHelper.IsIdentityNumberCorrectAsync(51529486516, "Ali", "Veli", 200);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task IsIdentityNumberCorrectAsyncWithEmptyNameAndSurname_ReturnFalse()
+        {
+            bool result = await TCIdentityNumberHelper.IsIdentityNumberCorrectAsync(51529486516, "", "", 2000);
+            Assert.IsFalse(result);
+        }
+    }
+}
